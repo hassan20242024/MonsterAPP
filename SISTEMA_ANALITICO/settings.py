7d185fe3fbce91d0121da20b6c2f0f8b1aecfb23
Marlon Andres Gomez Hassan <margomez@procaps.com.co>
2024-07-30 14:00:07 -0500

    revision static

--- SISTEMA_ANALITICO/settings.py
+++ SISTEMA_ANALITICO/settings.py
@@ -182,7 +182,7 @@ DATE_INPUT_FORMATS = ['%d-%B-%Y']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/home/hassan/static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [
 os.path.join(BASE_DIR, "static")
 ]
