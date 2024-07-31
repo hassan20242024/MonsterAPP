44d9503b728f216752fcc5b3e97af999af1c467d
Marlon Andres Gomez Hassan <margomez@procaps.com.co>
2024-07-31 18:14:53 -0500

    revision

--- SISTEMA_ANALITICO/urls.py
+++ SISTEMA_ANALITICO/urls.py
@@ -23,9 +23,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 #from Aplicaciones.perfiles.views import SignUpView, BienvenidaView,SignInView,SignOutView
 #urlpatterns = patterns("", url(r"^admin/", include(admin.site.urls)),)
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
+urlpatterns = [
 
+] + staticfiles_urlpatterns()
 
 urlpatterns = [
     ...
