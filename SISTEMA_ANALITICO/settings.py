10489e8a0e2009417809072ad057f895b7b6029b
Marlon Andres Gomez Hassan <margomez@procaps.com.co>
2024-07-31 22:25:55 -0500

    revision static

--- SISTEMA_ANALITICO/settings.py
+++ SISTEMA_ANALITICO/settings.py
@@ -187,7 +187,7 @@ STATICFILES_DIRS = [
 os.path.join(BASE_DIR, "static")
 ]
 
-STATIC_ROOT = "staticfiles/"
+STATIC_ROOT = "/var/www/MonsterAPP.com/static/"
 
 #SIMPLE_AUTOCOMPLETE = {'auth.user': {'search_field': 'username'}}
 
