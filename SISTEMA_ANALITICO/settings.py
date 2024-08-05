60f466ed9190f39374b53b9004d02d5092d9a2f1
Marlon Andres Gomez Hassan <margomez@procaps.com.co>
2024-08-05 13:23:01 -0500

    revision

--- SISTEMA_ANALITICO/settings.py
+++ SISTEMA_ANALITICO/settings.py
@@ -193,12 +193,14 @@ DATE_INPUT_FORMATS = ['%d-%B-%Y']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-os.path.join(BASE_DIR, "static")
-]
+STATIC_URL = "static/"
+import os
+#STATIC_ROOTN =
+STATICFILES_ROOT = os.path.join(BASE_DIR, "static")
+#os.path.join(BASE_DIR, "static")
+#]
 
-STATIC_ROOT = "staticfiles/"
+#STATIC_ROOT = "staticfiles/"
 
 #SIMPLE_AUTOCOMPLETE = {'auth.user': {'search_field': 'username'}}
 
