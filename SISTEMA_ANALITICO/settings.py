9a12b3168c7f5dd9ce8775e6be4dde5dafa51514
Marlon Andres Gomez Hassan <margomez@procaps.com.co>
2024-08-05 20:03:54 -0500

    para fines de produccion

--- SISTEMA_ANALITICO/settings.py
+++ SISTEMA_ANALITICO/settings.py
@@ -197,14 +197,8 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [
 os.path.join(BASE_DIR, "static")
 ]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATIC_TEMP = os.path.join(BASE_DIR, "static")
-os.makedirs(STATIC_TEMP, exist_ok=True)
-os.makedirs(STATIC_ROOT, exist_ok=True)
-
+STATIC_ROOT = "staticfiles/"
 
 #SIMPLE_AUTOCOMPLETE = {'auth.user': {'search_field': 'username'}}
 
